c277ad37feca3f26665a819f6908b992eb4b977a
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-17 19:36:20 -0400

    Fixed tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,24 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
-      extend: {
-        colors: {
-          night: {
-            mare: {
-              DEFAULT: "#141414",
-              2: "#0C0C0C",
-            },
-            mist: {
-              DEFAULT: "#AE8BFF",
-              2: "#8E5DFF",
-            },
-            sky: {
-              dawn: "#E0E0E0",
-              noon: "#BDBDBD",
-              dusk: "#828282",
-            },
-            hawk: {
-              DEFAULT: "#1C1C1C",
-              100: "#282828",
-            },
+      colors: {
+        night: {
+          mare: {
+            DEFAULT: "#141414",
+            2: "#0C0C0C",
+          },
+          mist: {
+            DEFAULT: "#AE8BFF",
+            2: "#8E5DFF",
+          },
+          sky: {
+            dawn: "#E0E0E0",
+            noon: "#BDBDBD",
+            dusk: "#828282",
+          },
+          hawk: {
+            DEFAULT: "#1C1C1C",
+            100: "#282828",
           },
         },
       },
