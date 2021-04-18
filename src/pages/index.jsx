5f5c8d32b1244c94da0499558addc887d18f2db3
TrustedMercury <kevin.jt2007@gmail.com>
2021-04-18 13:32:16 -0400

    Added second screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,54 @@
 import Navbar from "../components/core/Navbar";
+import FeatureSquare from "../components/index/FeatureSquare";
 
 const Index = (props) => {
+  const featureSquareList = [
+    {
+      icon: "fas fa-lock",
+      label: "Secure",
+      description:
+        "Turbo uses modern technology and is 100% opensource. You know exactly what is going on behind the scenes.",
+      className: "rounded-tl-md",
+    },
+    {
+      icon: "fas fa-bolt",
+      label: "Efficient",
+      description:
+        "Turbo uses significantly less memory and CPU compared to other leading web browsers like Chrome and Edge.",
+    },
+    {
+      icon: "fas fa-sparkles",
+      label: "Intuitive",
+      description:
+        "Turbo is fully keyboard navigationable. You can also enter custom hex codes to customize the browser to your liking.",
+      className: "rounded-tr-md",
+    },
+    {
+      icon: "fas fa-badge-dollar",
+      label: "Freemium",
+      description:
+        "Turbo is completely free to use, with no ads or interruptions! We also plan on adding a premium subscription.",
+      className: "rounded-bl-md",
+    },
+    {
+      icon: "fas fa-code-branch",
+      label: "Opensource",
+      description:
+        "Turbo is completely opensource. Anyone can contribute and add features they would like to see in their browser.",
+    },
+    {
+      icon: "fas fa-box-full",
+      label: "Powerful",
+      description:
+        "Packed with features, Turbo is constantly updated to make your development process more productive!",
+      className: "rounded-br-md",
+    },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen h-full bg-night-mare-2">
       <Navbar />
-      <div className="flex flex-col items-center justify-center py-48 w-full bg-night-mare-2">
+      <div className="flex flex-col items-center justify-center py-56 w-full bg-night-mare-2">
         <div className="flex flex-col items-center justify-center space-y-8">
           <div className="flex flex-col items-center justify-center max-w-3xl space-y-4 text-center">
             <h1 className="title-shadow font-inter font-extrabold text-8xl text-gray-200">
@@ -26,6 +70,19 @@ const Index = (props) => {
               Join the waitlist <i className="far fa-external-link-alt ml-1" />
             </h1>
           </a>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center py-32 w-full bg-night-mare">
+        <div className="grid grid-cols-3 gap-2">
+          {featureSquareList.map((feature, index) => (
+            <FeatureSquare
+              key={index}
+              icon={feature.icon}
+              label={feature.label}
+              description={feature.description}
+              className={feature.className}
+            />
+          ))}
         </div>
       </div>
     </div>
