2b9014069554a4f6c3c848890a20ec8c9516e49b
Robert <treboryx@gmail.com>
2021-04-26 17:43:35 +0300

    persist user preference to hide banner

--- src/components/core/Banner.jsx
+++ src/components/core/Banner.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const Banner = (props) => {
   return (
     <motion.div
-      className="flex flex-col md:flex-row items-center justify-center md:justify-between px-10 lg:px-40 2xl:px-72 space-y-4 md:space-y-0 h-32 md:h-20 w-full bg-mist-2 overflow-hidden"
+      className="flex flex-col items-center justify-center w-full h-32 px-10 space-y-4 overflow-hidden md:flex-row md:justify-between lg:px-40 2xl:px-72 md:space-y-0 md:h-20 bg-mist-2"
       exit="exit"
       variants={{
         exit: {
@@ -12,34 +12,34 @@ const Banner = (props) => {
         },
       }}
     >
-      <div className="flex flex-row items-center justify-start self-start md:self-auto space-x-2 md:space-x-2 xl:space-x-4">
+      <div className="flex flex-row items-center self-start justify-start space-x-2 md:self-auto md:space-x-2 xl:space-x-4">
         <div
           href="https://twitter.turbobrowser.io"
-          className="flex flex-row items-center justify-center p-2 bg-mist-3 rounded-md"
+          className="flex flex-row items-center justify-center p-2 rounded-md bg-mist-3"
           target="_blank"
         >
           <i className="twa twa-sparkles md:text-md lg:text-sm xl:text-xl" />
         </div>
-        <h1 className="font-inter font-medium text-xs md:text-xs lg:text-xs xl:text-lg 2xl:text-lg 3xl:text-xl text-gray-100">
+        <h1 className="text-xs font-medium text-gray-100 font-inter md:text-xs lg:text-xs xl:text-lg 2xl:text-lg 3xl:text-xl">
           Turbo is opensource and we're building it in public on Twitter and
           Discord!
         </h1>
       </div>
-      <div className="flex flex-row items-center justify-end space-x-3 md:space-x-3 lg:space-x-5 w-full md:w-auto">
+      <div className="flex flex-row items-center justify-end w-full space-x-3 md:space-x-3 lg:space-x-5 md:w-auto">
         <a
           href="https://twitter.turbobrowser.io"
           className="banner-button flex flex-row items-center justify-center px-3 lg:px-4 xl:px-6 py-2 w-full md:w-auto bg-gray-50 rounded-md transform hover:scale-[1.02] duration-700"
-          onClick={() => props.setBannerOpen(false)}
+          onClick={() => props.set()}
           target="_blank"
         >
-          <h1 className="font-inter font-medium md:text-sm lg:text-xs xl:text-lg text-mist-2">
+          <h1 className="font-medium font-inter md:text-sm lg:text-xs xl:text-lg text-mist-2">
             Learn More
           </h1>
         </a>
         <div
           href="https://twitter.turbobrowser.io"
           className="banner-button flex flex-row items-center justify-center px-4 md:px-3 xl:px-4 md:py-2 h-full md:h-auto bg-gray-50 cursor-pointer rounded-md transform hover:scale-[1.02] duration-700"
-          onClick={() => props.setBannerOpen(false)}
+          onClick={() => props.set()}
           target="_blank"
         >
           <i className="far fa-times md:text-sm lg:text-xs xl:text-lg text-mist-2" />
